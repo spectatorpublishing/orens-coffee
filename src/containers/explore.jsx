ae2a0a1c1291a7eaf40fd228e15fcb0679c6cbbf
lauracastrovenegas <61126997+lauracastrovenegas@users.noreply.github.com>
2022-02-08 17:08:37 -0500

    added card to page and added data with dynamic rendering

--- src/containers/explore.jsx
+++ src/containers/explore.jsx
@@ -6,6 +6,9 @@ import useWindowDimensions from '../hooks/window.js'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import CoffeeCard from '../components/coffee-card';
+import { coffee_data } from '../data/coffeedata';
+import { orens_info } from '../data/coffeedata';
 
 const redPinUrl = "https://sbs-assets.s3.amazonaws.com/orens-coffee/pushpin+3.png"
 const map = {
@@ -98,9 +101,8 @@ const Card = styled.div`
 	top: 50%;
     transform: translate(-50%, -50%);
 	z-index: 999;
-	width: 60vw;
+	width: 75vw;
 	height: 50vh;
-	background-color: ${theme.colors.cream};
 `
 
 const CardPopUp = ({ cb, children, visible }) => (
@@ -114,7 +116,7 @@ const CardPopUp = ({ cb, children, visible }) => (
 // pops up at the center of the page and can be toggled
 // clicking the background (i.e the transparent Modal
 // component covering the entire view).
-const withCard = Card => ({ xCord, yCord }) => {
+const withCard = Card => ({ xCord, yCord, location }) => {
 	const [ visible, setVisible ] = useState(false)
 	const toggle = () => setVisible(!visible)
 
@@ -127,7 +129,15 @@ const withCard = Card => ({ xCord, yCord }) => {
 				onClick={toggle} />
 
 			<CardPopUp cb={toggle} visible={visible}>
-				<Card visible={visible} />
+				<Card visible={visible}>
+				{(location === "Kenya") ? 
+						<CoffeeCard data={coffee_data[0]} onClose={toggle}/>
+						: (location === "Manita") ? 
+						<CoffeeCard data={coffee_data[1]} onClose={toggle}/>
+						: (location === "Sumatra") ? 
+						<CoffeeCard data={coffee_data[2]} onClose={toggle}/>
+						: <CoffeeCard data={orens_info[0]} onClose={toggle}/>}
+					</Card>
 			</CardPopUp>
 		</>
 	)
@@ -147,10 +157,10 @@ export default function Explore(props) {
 		<>
 			<BackButton onClick={props.onClick}><FontAwesomeIcon icon={faChevronLeft}/> Back</BackButton>
 			<Map src={map.url} />
-			<Kenya xCord={marks.kenya[0]} yCord={marks.kenya[1]} />
-			<Manita xCord={marks.manita[0]} yCord={marks.manita[1]} />
-			<Sumatra xCord={marks.sumatra[0]} yCord={marks.sumatra[1]} />
-			<NYC xCord={marks.nyc[0]} yCord={marks.nyc[1]} />
+			<Kenya xCord={marks.kenya[0]} yCord={marks.kenya[1]} location="Kenya" />
+			<Manita xCord={marks.manita[0]} yCord={marks.manita[1]} location="Manita"/>
+			<Sumatra xCord={marks.sumatra[0]} yCord={marks.sumatra[1]} location="Sumatra"/>
+			<NYC xCord={marks.nyc[0]} yCord={marks.nyc[1]} location="NYC"/>
 			<Down><FontAwesomeIcon icon={faChevronDown}/></Down>
 		</>
 	)
